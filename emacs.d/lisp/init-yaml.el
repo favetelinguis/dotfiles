3fb82371702307d0ce8bbc58ad84f8a9ceeb45b5
Henrik Larsson <henriklarsson@Henriks-MacBook-Pro.local>
2023-04-23 20:03:05 +0200

    Fix yaml mode

--- emacs.d/lisp/init-yaml.el
+++ emacs.d/lisp/init-yaml.el
@@ -1,3 +1,9 @@
-(straight-use-package 'yaml)
+(straight-use-package 'yaml-mode)
+;; (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+;; Dont think this is needed (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+;; Mode dont bind RET to newline and indent by default
+;; (add-hook 'yaml-mode-hook
+;; 	  '(lambda ()
+;; 	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
 (provide 'init-yaml)
