8792a298348f5628555f3aedde40b3502f0410cb
Henrik Larsson <henriklarsson@Henriks-MacBook-Pro.local>
2023-04-24 20:16:41 +0200

    Setup guile scheme

--- emacs.d/lisp/init-lisps.el
+++ emacs.d/lisp/init-lisps.el
@@ -1,10 +1,18 @@
+;; For clojure
 (straight-use-package 'clojure-mode)
 (straight-use-package 'cider)
+;; Enable subword mode in cider repl
+(with-eval-after-load 'cider
+  (add-hook 'cider-repl-mode-hook 'subword-mode))
 
+;; For scheme
+(straight-use-package 'geiser)
+(straight-use-package 'geiser-guile)
+
+;; Setup paredit for lisp
 (straight-use-package 'paredit)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
-(with-eval-after-load 'cider
-  (add-hook 'cider-repl-mode-hook 'subword-mode))
+(add-hook 'geiser-mode-hook 'paredit-mode)
 
 (provide 'init-lisps)
