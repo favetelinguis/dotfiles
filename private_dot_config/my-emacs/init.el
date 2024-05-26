6fd0035dd717e6da86c03abda33be5101aeb26fb
Henrik Larsson <reg@henkelarsson.se>
2024-05-26 16:08:53 +0200

    Group ibuffer by vc dir

--- private_dot_config/my-emacs/init.el
+++ private_dot_config/my-emacs/init.el
@@ -382,3 +382,24 @@
 ;;   :config
 ;;   (treesit-auto-add-to-auto-mode-alist 'all)
 ;;   (global-treesit-auto-mode))
+
+(use-package ibuffer-vc
+  :config
+  ;; Always group by VC
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-vc-set-filter-groups-by-vc-root)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic))))
+  ;; Include VC status
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                (vc-status 16 16 :left)
+                " "
+                vc-relative-file))))
