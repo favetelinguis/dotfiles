40dd5bc3e5a09f00261af57733442cb4de1e234c
Henrik Larsson <henke@henkelarsson.se>
2026-04-07 20:49:16 +0200

    fix git signs

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -14,49 +14,49 @@ return {
 			end
 
 			-- Navigation
-			map("n", "]c", function()
+			map("n", "]h", function()
 				if vim.wo.diff then
 					vim.cmd.normal({ "]c", bang = true })
 				else
 					gitsigns.nav_hunk("next")
 				end
-			end, { desc = "Jump to next git [c]hange" })
+			end, { desc = "Jump to next git [h]unk" })
 
-			map("n", "[c", function()
+			map("n", "[h", function()
 				if vim.wo.diff then
 					vim.cmd.normal({ "[c", bang = true })
 				else
 					gitsigns.nav_hunk("prev")
 				end
-			end, { desc = "Jump to previous git [c]hange" })
+			end, { desc = "Jump to previous git [h]unk" })
 
 			-- Actions
 			-- visual mode
-			-- map("v", "<leader>hs", function()
+			-- map("v", "<leader>vs", function()
 			-- 	gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 			-- end, { desc = "git [s]tage hunk" })
-			-- map("v", "<leader>hr", function()
+			-- map("v", "<leader>vr", function()
 			-- 	gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 			-- end, { desc = "git [r]eset hunk" })
 			-- normal mode
-			map("n", "<leader>hf", require("fzf-lua").git_hunks, "FZF hunks")
-			-- map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
-			-- map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
-			-- map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
-			-- map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
-			map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
-			map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "git preview hunk [i]nline" })
-			map("n", "<leader>hb", function()
+			map("n", "<leader>vf", require("fzf-lua").git_hunks, { desc = "FZF hunks" })
+			-- map("n", "<leader>vs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
+			-- map("n", "<leader>vr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
+			-- map("n", "<leader>vS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
+			-- map("n", "<leader>vR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
+			map("n", "<leader>vp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
+			map("n", "<leader>vi", gitsigns.preview_hunk_inline, { desc = "git preview hunk [i]nline" })
+			map("n", "<leader>vb", function()
 				gitsigns.blame_line({ full = true })
 			end, { desc = "git [b]lame line" })
-			map("n", "<leader>hd", gitsigns.diffthis, { desc = "git [d]iff against index" })
-			map("n", "<leader>hD", function()
+			map("n", "<leader>vd", gitsigns.diffthis, { desc = "git [d]iff against index" })
+			map("n", "<leader>vD", function()
 				gitsigns.diffthis("@")
 			end, { desc = "git [D]iff against last commit" })
-			map("n", "<leader>hQ", function()
+			map("n", "<leader>vQ", function()
 				gitsigns.setqflist("all")
 			end)
-			map("n", "<leader>hq", gitsigns.setqflist)
+			map("n", "<leader>vq", gitsigns.setqflist)
 			-- Toggles
 			map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
 			map("n", "<leader>tw", gitsigns.toggle_word_diff)
